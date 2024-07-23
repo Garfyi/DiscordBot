4d7ddb12c99d5f2c18656e262f7c486d99fcdada
Garfyi <garmaliful@gmail.com>
2024-07-23 11:08:14 -0400

    Made it work

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
+# bot.py
+import os
+
 import discord
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
-intents.message_content = True
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('MTI2NTMwNDc5Mzk4Mzc1MDE2NA.GJ8Z23.wSj1ACyLxdBwho0v2vnNleOjhU-WMB9-MBoWTM')
+client.run(TOKEN)
