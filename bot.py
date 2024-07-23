b6b4062bdd5b9ef1d3ac31d4a1e7f0ab18a7f5b1
Garfyi <garmaliful@gmail.com>
2024-07-23 13:46:10 -0400

    manually add profiles and added showGBP command

--- bot.py
+++ bot.py
@@ -25,7 +25,35 @@ async def on_ready():
     await bot.tree.sync()
     print(f'logged in as {bot.user} (ID: {bot.user.id})')
 
+@bot.tree.command(name="addgbp",description="add good boy points to member")
+async def addgbp(interaction:discord.Interaction, member: discord.Member, amount : int):
+    if os.path.isfile(f'{str(member.id)}.txt'):
+        f = open(f'{str(member.id)}.txt', "r")
+        gbp = int(f.read())
+        f.close()
+        f = open(f'{str(member.id)}.txt', "w+")
+        f.write(str(gbp + amount))
+    else:
+        await interaction.response.send_message(f'{member} is does not have a GPB profile')
+        return
+        
+    await interaction.response.send_message(f'{member} now has {f.read()} Good Boy Points!')
+    f.close()
 
+@bot.tree.command(name="showgbp",description="show member's good boy points")
+async def showgbp(interaction:discord.Interaction, member: discord.Member):
+    if os.path.isfile(f'{str(member.id)}.txt'):
+        f = open(f'{str(member.id)}.txt', "r")
+        gbp = int(f.read())
+    else:
+        await interaction.response.send_message(f'{member} is does not have a GPB profile')
+        return
+        
+    await interaction.response.send_message(f'{member} now has {gbp} Good Boy Points!')
+    f.close()
+
+# If users are added dynamically it's going to take up a lot of space
+""""
 @bot.tree.command(name="addgbp",description="add good boy points to member")
 async def addgbp(interaction:discord.Interaction, member: discord.Member, amount : int):
     if os.path.isfile(f'{str(member.id)}.txt'):
@@ -40,4 +68,5 @@ async def addgbp(interaction:discord.Interaction, member: discord.Member, amount
         
     await interaction.response.send_message(f'{member} now has {f.read()} Good Boy Points!')
     f.close()
+"""
 bot.run(TOKEN)
