b8370de4476ab36efd8ee160e185661e890b32bc
Garfyi <garmaliful@gmail.com>
2024-07-23 11:32:04 -0400

    added response event

--- bot.py
+++ bot.py
@@ -2,9 +2,14 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+#import random
 
 intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,5 +19,13 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$allo'):
+        await message.channel.send('oui?')
 
 client.run(TOKEN)
