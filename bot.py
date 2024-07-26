7717161ca406bce55fc16f17b84ec34c7c779b14
Garfyi <garmaliful@gmail.com>
2024-07-26 11:00:28 -0400

    Update bot.py
    fixed naming convention
    imported googletrans
    added translate contextmenu to english

--- bot.py
+++ bot.py
@@ -1,38 +1,40 @@
 # bot.py
 import os
 import os.path
-import random
 from collections import OrderedDict
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+import googletrans
+from googletrans import Translator
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='g.', intents=discord.Intents.all())
+BOT = commands.Bot(command_prefix='g.', intents=discord.Intents.all())
+TRANSLATOR = Translator()
 
 # When bot is online
-@bot.event
+@BOT.event
 async def on_ready():
-    await bot.tree.sync()
-    print(f'logged in as {bot.user} (ID: {bot.user.id})')
+    await BOT.tree.sync()
+    print(f'logged in as {BOT.user} (ID: {BOT.user.id})')
 
 
 # Anytime a message is sent in any channel
-@bot.event
+@BOT.event
 async def on_message(ctx):
-    if ctx.author == bot.user:
+    if ctx.author == BOT.user:
         return
     
     if "g.kill" == ctx.content:
         print(f'Some dude killed the bot')
-        await bot.close()
+        await BOT.close()
 
     if "g.shame" == ctx.content:
-        await shameChat(ctx)
+        await shame_chat(ctx)
         return
 
 
@@ -42,7 +44,8 @@ async def on_message(ctx):
 
 # Shames user function when used as chat message
 
-async def shameChat(ctx):
+async def shame_chat(ctx):
+
     try:
         message = await ctx.channel.fetch_message(ctx.reference.message_id)
     except:
@@ -59,8 +62,9 @@ async def shameChat(ctx):
     return
 
 
-@bot.tree.context_menu(name="shame")
+@BOT.tree.context_menu(name="shame")
 async def shame(interaction:discord.Interaction, message:discord.Message):
+
     shamed = f'{message.author} was shamed after saying: {message.content}'
     
     f = open(f'Shame_data/data.txt', "a")
@@ -69,12 +73,27 @@ async def shame(interaction:discord.Interaction, message:discord.Message):
 
     await interaction.response.send_message(shamed)
 
+
+#
+# TRANSLATION SECTION
+#
+
+# Translate the message to english from the context menu
+@BOT.tree.context_menu(name="translate")
+async def translate(interaction:discord.Interaction, message:discord.Message):
+
+    src = TRANSLATOR.detect(message.content)
+    text = TRANSLATOR.translate(message.content, dest='en').text
+
+    await interaction.response.send_message(f'{message.author} said : "{text}" in {googletrans.LANGUAGES[f'{src.lang}']}')
+
+
 #
 # GOOD BOY POINTS SECTION
 #
 
 # Adds gbp to a user if they have a profile
-@bot.tree.command(name="givegbp",description="give good boy points to member min:-3 max:3")
+@BOT.tree.command(name="givegbp",description="give good boy points to member min:-3 max:3")
 async def givegbp(interaction:discord.Interaction, member: discord.Member, amount : int):
 
     if interaction.user == member:
@@ -106,8 +125,9 @@ async def givegbp(interaction:discord.Interaction, member: discord.Member, amoun
     f.close()
 
 # Shows how many good boy points a user has
-@bot.tree.command(name="showgbp",description="show member's good boy points")
+@BOT.tree.command(name="showgbp",description="show member's good boy points")
 async def showgbp(interaction:discord.Interaction, member: discord.Member):
+
     if os.path.isfile(f'GBP_data/{member.id}.txt'):
         f = open(f'GBP_data/{member.id}.txt', "r")
         gbp = int(f.read())
@@ -120,8 +140,9 @@ async def showgbp(interaction:discord.Interaction, member: discord.Member):
     
 
 # Adds member text file to store profile info
-@bot.tree.command(name="creategbp",description="creates gbp profile for command caller")
+@BOT.tree.command(name="creategbp",description="creates gbp profile for command caller")
 async def creategbp(interaction:discord.Interaction):
+
     if os.path.isfile(f'{interaction.user.id}.txt') is False:
         f = open(f'GBP_data/{interaction.user.id}.txt', "w+")
         f.write(f'0')
@@ -134,15 +155,16 @@ async def creategbp(interaction:discord.Interaction):
 
 
 # Displays everybody's numbre of Good Boy Points as a leaderboard
-@bot.tree.command(name="leaderboard",description="shows gbp leaderboard")
+@BOT.tree.command(name="leaderboard",description="shows gbp leaderboard")
 async def leaderboard(interaction:discord.Interaction):
+
     files = os.listdir('./GBP_data')
     dict = OrderedDict()
     board = 'Good Boy Point leaderboard\n'
 
     for file in files :
         amount = open(f'GBP_data/{file}', 'r')
-        dict[bot.get_user(int(file.removesuffix('.txt'))).name] = int(amount.read())
+        dict[BOT.get_user(int(file.removesuffix('.txt'))).name] = int(amount.read())
         amount.close()
 
     sortedDict  = OrderedDict(sorted(dict.items(), key=lambda x: x[1], reverse=True))
@@ -174,4 +196,4 @@ async def addgbp(interaction:discord.Interaction, member: discord.Member, amount
     await interaction.response.send_message(f'{member} now has {f.read()} Good Boy Points!')
     f.close()
 """
-bot.run(TOKEN)
+BOT.run(TOKEN)
